1ce6ade48f16c4cd64f3bbed0d8983b015bf1248
Maksym Shamko <max.shamko25@gmail.com>
2021-10-07 13:30:39 +0300

    working

--- index.ts
+++ index.ts
@@ -1,4 +1,16 @@
-export { TRIX } from './src/trix';
-export { SMA } from './src/sma';
-export { RSI } from './src/rsi';
-export { EMA } from './src/ema';
+import { TRIX } from './src/trix';
+import { SMA } from './src/sma';
+import { RSI } from './src/rsi';
+import { EMA } from './src/ema';
+
+export { TRIX };
+export { SMA };
+export { RSI };
+export { EMA };
+
+export default {
+  TRIX,
+  SMA,
+  RSI,
+  EMA,
+};
